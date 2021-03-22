585b772f3ca8d2e22e2813fa16064e14600c8410
cabauman <cab452005@gmail.com>
2021-03-22 15:32:19 +0900

    set up basic test environment

---
+++ tests/unit/stub.test.ts
@@ -0,0 +1,15 @@
+import { expect } from 'chai';
+
+describe('calculate B', function () {
+  it('add', function () {
+    const result = 7;
+    expect(result).equal(7);
+  });
+});
+
+describe('calculate A', function () {
+  it('add', function () {
+    const result = 7;
+    expect(result).equal(7);
+  });
+});
