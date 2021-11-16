d70fbac22587ad1ec8e22426341db8c46306f2a4
cabauman <cab452005@gmail.com>
2021-11-16 12:30:52 +0900

    chore: fix commitlint-check script
    It was reporting correctly but not failing pipeline.

--- commitlint-pipeline-check.sh
+++ commitlint-pipeline-check.sh
@@ -3,19 +3,25 @@
 COMMITS=$(git log origin/$BITBUCKET_PR_DESTINATION_BRANCH..$BITBUCKET_BRANCH --oneline --reverse | cut -d " " -f 1)
 status=0
 commitlint_status=0
+
 for COMMIT in $COMMITS
 do
     MSG=$(git show --format="%B" -s $COMMIT)
-    echo "$MSG" | npx commitlint -V || (commitlint_status=1 && status=1)
+    echo "$MSG" | npx commitlint -V
+    if [[ $? -eq 1 ]]
+    then
+        commitlint_status=1
+        status=1
+    fi
 
-    if  [[ $MSG == wip:* ]]
+    if [[ $MSG == wip:* ]]
     then
         status=1
         echo "The 'wip' commit type is permittable during development, but we're failing"
         echo "this PR pipeline to ensure it doesn't find its way into the destination branch."
     fi
 
-    if  [[ $MSG == fixup!* ]]
+    if [[ $MSG == fixup!* ]]
     then
         status=1
         echo "'fixup!' is permittable during development, but we're failing"
@@ -23,7 +29,8 @@ do
     fi
     echo ------------------
 done
-if  [[ $commitlint_status == 1 ]]
+
+if [[ $commitlint_status == 1 ]]
 then
     echo "One or more commit messages were formatted incorrectly. Please see CONTRIBUTING.md for details."
     echo "This should have been caught locally by commit hooks."
